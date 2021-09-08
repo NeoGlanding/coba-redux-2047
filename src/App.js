b3843715dc08f32b82b02ed18756beb0d070741e
NeoGlanding <neoglanding@protonmail.com>
2021-09-08 20:29:53 +0700

    fetch api ke redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './component/FunctionalComponents';
+import ReduxThunk from './component/ReduxThunk';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <ReduxThunk />
     </div>
   );
 }
--- src/store/redux.js
+++ src/store/redux.js
@@ -1,20 +1,13 @@
 import { createStore } from 'redux';
 
 const stateAwal = {
-    counter: [{id: 2 ,angka: 1}, {id: 1, angka: 2}]
+    data: []    
 }
 
 const reducer = (state = stateAwal, action) => {
-    if (action.type === 'INCREMENT_1') {
-    }
-
-    if (action.type === 'INCREMENT_2') {
-        let copyCounter = [...state.counter];
-        let index = state.counter.findIndex(el => el.id === action.payload.id);
-        copyCounter[index] = {...copyCounter[index], angka: copyCounter[index].angka + action.payload.angka};
-
+    if (action.type === 'GET_API') {
         return {
-            counter: copyCounter
+            data: [...state.data, ...action.payload]
         }
     }
 
